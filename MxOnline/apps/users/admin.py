8d4e3c3c55fe808019bc82c87189d68340ccbd1c
chenguanyou <btmeiju@126.com>
2018-01-27 20:43:21 +0800

    remove

--- MxOnline/apps/users/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# from .models import UserProFile
-# # Register your models here.
-#
-# class UserProFileAdmin(admin.ModelAdmin):
-#     pass
-#
-# admin.site.register(UserProFile, UserProFileAdmin)
--- MxOnline/apps/utils/mixin_utils.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
-
-
-class LoginRequiredMIxin(object):
-    @method_decorator(login_required(login_url='/login/'))
-    def dispatch(self, request, *args, **kwargs):
-        return super(LoginRequiredMIxin, self).dispatch(request, *args, **kwargs)
